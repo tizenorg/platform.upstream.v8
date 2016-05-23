2386038bce5d0a5a06f1c1fda9b9ad0cb384b2ec
Youngsoo Choi <kenshin.choi@samsung.com>
2016-05-23 16:52:05 +0900

    Revert "Make a command 'git submodule' work in spec file"
    This reverts commit d5270085726338b2e3320b7402420ce9dba764b1.
    
    The command '$git submodule' is not available on obs server.
    
    Change-Id: I00ce70b562c81e37b72691cc1c735625bdaee9c1

--- build/get_third_party_modules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function download_third_party_modules() {
-  git config --global http.sslVerify false
-  git submodule update --init
-  pushd build/gyp; git reset --hard 08429da7; popd;
-}
-
-download_third_party_modules
